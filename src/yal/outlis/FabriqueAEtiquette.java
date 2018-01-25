f4a75a2c32f3ceb88e5d683a60fa68b5d912f565
Saunier <guillaume.saunier@outlook.fr>
2018-01-25 15:28:53 +0100

    fabrique a étiquette

---
+++ src/yal/outlis/FabriqueAEtiquette.java
@@ -0,0 +1,43 @@
+package yal.outlis;
+
+public class FabriqueAEtiquette {
+
+	private FabriqueAEtiquette instance = new FabriqueAEtiquette();
+	private int indexSi;
+	private int indexDiv0;
+	
+	private FabriqueAEtiquette() {
+		indexSi = 0;
+		indexDiv0 = 0;
+	};
+	
+	public FabriqueAEtiquette getInstance() {
+		return instance;
+	}
+
+	public int getIndexSi() {
+		return indexSi;
+	}
+
+	public void setIndexSi(int indexSi) {
+		this.indexSi = indexSi;
+	}
+
+	public int getIndexDiv0() {
+		return indexDiv0;
+	}
+
+	public void setIndexDiv0(int indexDiv0) {
+		this.indexDiv0 = indexDiv0;
+	}
+	
+	public void addIndexSi() {
+		indexSi++;
+	}
+	
+	public void addIndexDiv0() {
+		indexDiv0++;
+	}
+	
+	
+}
