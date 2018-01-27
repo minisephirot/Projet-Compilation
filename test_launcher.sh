0495be821656c6bcf4f2d7b222418b6dcb7d7a29
minisephirot <minisephirot@hotmail.fr>
2018-01-27 18:25:36 +0100

    ajout des tests div 0

--- test_launcher.sh
+++ test_launcher.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+> restest.txt
 for fichier in `ls test | grep ".yal"`
 do
+	echo Doing $fichier ...
 	echo ---------------------------------- >> restest.txt
-	java -jar yal.jar test/$fichier 2>> restest.txt
+	java -jar yal.jar test/$fichier  2>> restest.txt
 	java -jar Mars4_5.jar dec v1 yal.mips >> restest.txt
 done
 echo FINISHED
