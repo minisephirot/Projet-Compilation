cdd7ccf08225e5ae7a7c470c25206634924884a3
minisephirot <minisephirot@hotmail.fr>
2018-02-07 14:44:16 +0100

    modif shell & gitignore

--- test_launcher.sh
+++ test_launcher.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 > restest.txt
-for fichier in `ls test | grep ".yal"`
+for fichier in `ls test -a | cut -d "." -f 1`
 do
 	echo Doing $fichier ...
 	echo ---------------------------------- >> restest.txt
-	java -jar yal.jar test/$fichier  2>> restest.txt
-	java -jar Mars4_5.jar dec v1 yal.mips >> restest.txt
+	java -jar yal.jar test/$fichier.yal  2>> restest.txt
+	java -jar Mars4_5.jar dec v1 $fichier.mips >> restest.txt
 done
 echo FINISHED
 
