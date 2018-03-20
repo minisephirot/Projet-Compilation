6a819ccae38a81ae0f36e0162846b8ff03391771
Nono1905 <nono1905@gmail.com>
2018-03-20 09:30:09 +0100

    Correction lire variables hors bloc

--- src/yal/arbre/instruction/Lire.java
+++ src/yal/arbre/instruction/Lire.java
@@ -1,6 +1,7 @@
 package yal.arbre.instruction;
 
 import yal.arbre.expression.idf.IDFVar;
+import yal.outils.EtiquetteFactory;
 
 public class Lire extends Instruction {
 
@@ -21,12 +22,29 @@ public class Lire extends Instruction {
 		StringBuilder sb = new StringBuilder();
 		//Décallage avec appel méthode getDécalage de idf
 		int decalage = idf.getDecalage();
+		String itr = EtiquetteFactory.getInstance().getItr();
 		sb.append("# Ecrire dans une variable \n");
 		sb.append("li $v0, 5\n");
 		sb.append("syscall\n");
 		sb.append("# sw au bon endroit\n");
-		sb.append("sw $v0, " + decalage + "($s7)\n");
-		
+		sb.append("# emplile \n");
+		sb.append("sw $v0, 0($sp)\n");
+		sb.append("addi $sp, $sp, -4\n");
+		sb.append("# Remonte le chainage\n");
+		sb.append("move $t8, $s7 \n");
+		sb.append(itr + ": \n");
+		sb.append("lw $v0, 4($t8) \n");
+		sb.append("addi $v1, $zero, " + idf.getNoBloc() + " \n");
+		sb.append("sub $v1, $v0, $v1\n");
+		sb.append("beqz $v1, fin" + itr + "\n");
+		sb.append("lw $t8, 8($t8) \n");
+		sb.append("j " + itr + " \n");
+		sb.append("fin" + itr + ": \n");
+		sb.append("#Dépile \n");
+		sb.append("addi $sp, $sp, 4\n");
+		sb.append("lw $v0, 0($sp)\n");
+		sb.append("sw $v0, " + decalage + "($t8)\n");
+
 		return sb.toString();
 	}
 
