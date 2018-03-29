c6aef78451bdf7c400892e6ac8a094715c965a2b
Nono1905 <nono1905@gmail.com>
2018-03-29 17:44:20 +0200

    Initialisation à 0 tableau

--- src/yal/arbre/DeclTab.java
+++ src/yal/arbre/DeclTab.java
@@ -38,20 +38,28 @@ public class DeclTab extends ArbreAbstrait {
     @Override
     public String toMIPS() {
         StringBuilder sb = new StringBuilder();
+        int indexEtiquette = EtiquetteFactory.getInstance().getIndexTant();
+        EtiquetteFactory.getInstance().addIndexTant();
         sb.append("# Allocation du tableau " + idf + " \n");
+
         sb.append("# Enregistre l'adresse de début du tableau \n");
         sb.append("sw $sp, " + decalage + "($s7) \n");
         sb.append(exp.toMIPS());
+
         sb.append("#Test si l'indice est négatif ou égal à 0\n");
         sb.append("blez $v0, indiceNegNul \n");
+
         sb.append("# Enregistre la taille du tableau dans la pile \n");
         sb.append("sw $v0, " + (decalage-4) + "($s7) \n");
-        sb.append("# Calcul le décalage du tabeau \n");
-        sb.append("addi, $t8, $zero, -4 \n");
-        sb.append("mult $v0, $t8\n");
-        sb.append("mflo $v0\n");
-        sb.append("# Allocation du tableau \n");
-        sb.append("add, $sp, $sp, $v0 \n");
+
+        sb.append("# Boucle Allocation du tableau \n");
+        sb.append("Tantque" + indexEtiquette + " :\n");
+        sb.append("beqz $v0, FinTantque" + indexEtiquette + "\n");
+        sb.append("addi, $v0, $v0, -1\t# taille tab -1 \n");
+        sb.append("lw $zero, ($sp)\t# mise à zéro de la case \n");
+        sb.append("addi, $sp, $sp, -4\t# pile -4 \n");
+        sb.append("b Tantque" + indexEtiquette + "\n");
+        sb.append("FinTantque" + indexEtiquette + ":\n");
 
         return sb.toString();
     }
